951f118bc3411e1b65df3295738c2f816e112c38
Edmund Aldrich de Leon <ext.edmund.aldrichdeleon@tieto.com>
2015-05-15 22:22:54 +0200

    added something

--- FoodDiary/ViewController.swift
+++ FoodDiary/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        println("Testing Git")
+        println("Testing Git. added something to your line")
+        
     }
 
     override func didReceiveMemoryWarning() {
