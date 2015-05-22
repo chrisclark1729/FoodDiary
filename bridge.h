f05ddb78b30e167c06bd5ab5a65454be889f610f
Chris Clark <chrismclark@chrismclark-macbookpro2.roam.corp.google.com>
2015-05-22 08:53:41 -0700

    Add AFNetworking Header file

---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  FoodDiary
+//
+//  Created by Chris Clark on 5/22/15.
+//  Copyright (c) 2015 Chris Clark. All rights reserved.
+//
+
+#ifndef FoodDiary_bridge_h
+#define FoodDiary_bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif
