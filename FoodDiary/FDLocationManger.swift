a3895727d6cafa28d45bc75f5fc2a313f757afbf
Chris Clark <chrismclark@chrismclark-macbookpro2.roam.corp.google.com>
2016-02-29 12:06:17 -0800

    Meeting.

---
+++ FoodDiary/FDLocationManger.swift
@@ -0,0 +1,111 @@
+//
+//  LocationManagerViewController.swift
+//  FoodDiary
+//
+//  Created by Chris Clark on 7/26/15.
+//  Copyright (c) 2015 Chris Clark. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+import Parse
+
+class FDLocationManager: NSObject, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    static let sharedLocation = FDLocationManager()
+    var lastKnownLatitude: Float = 0
+    var lastKnownLongitude: Float = 0
+    var lastKnownLocation: CLLocation?
+    var locationNameGuess = ""
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      //  var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        
+        self.lastKnownLocation = manager.location
+        print("Location Found")
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func refreshLocation() {
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError)
+    {
+        print("Error: " + error.localizedDescription)
+    }
+    
+    let placesClient = GMSPlacesClient()
+    
+    func getCurrentPlace()  {
+        
+        var likelihoodScore:Double = 0
+        
+        placesClient.currentPlaceWithCallback({ (placeLikelihoods, error) -> Void in
+            if error != nil {
+                print("Current Place error: \(error!.localizedDescription)")
+                return
+            }
+            
+            for likelihood in placeLikelihoods!.likelihoods {
+                if let likelihood = likelihood as? GMSPlaceLikelihood {
+                    let place = likelihood.place
+                    
+                    if likelihood.likelihood > likelihoodScore {
+                        likelihoodScore = likelihood.likelihood
+                        self.locationNameGuess = place.name
+                    }
+                    
+                    print("likelihood score is now: \(likelihoodScore)")
+                    
+             // print("Current Place name \(place.name) at likelihood \(likelihood.likelihood)")
+                }
+            }
+            
+            if likelihoodScore < 0.4 {
+                self.locationNameGuess = self.getLocationNameFromBackend()
+            }
+        })
+        
+    }
+    
+    func getLocationNameFromBackend() -> String {
+
+        let locationNameSuggestions = FoodDiaryEntry.getLocationSuggestions(PFGeoPoint(location: FDLocationManager.sharedLocation.lastKnownLocation))
+        
+        var locationName = ""
+        
+        if locationNameSuggestions.count > 0 {
+            locationName = locationNameSuggestions[0]
+        }
+        
+        return locationName
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
--- FoodDiary/IngredientAddTableViewController.swift
+++ FoodDiary/IngredientAddTableViewController.swift
@@ -15,12 +15,61 @@ class IngredientAddTableViewController: UITableViewController {
     var ingredient: PFObject?
     var foodDiaryDetail = [FoodDiaryDetail]()
     
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var fatGramsLabel: UILabel!
+    @IBOutlet weak var saturatedFatGramsLabel: UILabel!
+    @IBOutlet weak var cholesterolLabel: UILabel!
+    @IBOutlet weak var sodiumLabel: UILabel!
+    @IBOutlet weak var gramsCarbsLabel: UILabel!
+    @IBOutlet weak var gramsFiberLabel: UILabel!
+    @IBOutlet weak var gramsSugarLabel: UILabel!
+    @IBOutlet weak var gramsProteinLabel: UILabel!
+    
     
     @IBOutlet weak var numberOfServingsTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(foodDiaryDetail)
+
+        let calories = ingredient!["calories"]!
+        let fatGrams = ingredient!["gramsFat"]!
+        let saturatedFatGrams = ingredient!["gramsSaturatedFat"]!
+        let cholesterol = ingredient!["gramsCholesterol"]!
+        let sodium = ingredient!["gramsSodium"]!
+        let gramsCarbs = ingredient!["gramsCarbs"]!
+        let fiber = ingredient!["gramsFiber"]!
+        let sugar = ingredient!["gramsSugar"]!
+        let protein = ingredient!["gramsProtein"]!
+        var caloriesDisplay: Int = 0
+        var fatGramsDisplay: Int = 0
+        var saturatedFatGramsDisplay: Int = 0
+        var cholesterolDisplay: Int = 0
+        var sodiumDisplay: Int = 0
+        var gramsCarbsDisplay: Int = 0
+        var fiberDisplay: Int = 0
+        var sugarDisplay: Int = 0
+        var proteinDisplay: Int = 0
+        var multiplier = 1
+        
+        caloriesDisplay = (calories as? Int)! * multiplier
+        fatGramsDisplay = (fatGrams as? Int)! * multiplier
+        saturatedFatGramsDisplay = (saturatedFatGrams as? Int)! * multiplier
+        cholesterolDisplay = (cholesterol as? Int)! * multiplier
+        sodiumDisplay = (sodium as? Int)! * multiplier
+        gramsCarbsDisplay = (gramsCarbs as? Int)! * multiplier
+        fiberDisplay = (fiber as? Int)! * multiplier
+        sugarDisplay = (sugar as? Int)! * multiplier
+        proteinDisplay = (protein as? Int)! * multiplier
+        
+        self.caloriesLabel.text = String(caloriesDisplay)
+        self.fatGramsLabel.text = String(fatGramsDisplay)
+        self.saturatedFatGramsLabel.text = String(saturatedFatGramsDisplay)
+        self.cholesterolLabel.text = String(cholesterolDisplay)
+        self.sodiumLabel.text = String(sodiumDisplay)
+        self.gramsCarbsLabel.text = String(gramsCarbsDisplay)
+        self.gramsFiberLabel.text = String(fiberDisplay)
+        self.gramsSugarLabel.text = String(sugarDisplay)
+        self.gramsProteinLabel.text = String(proteinDisplay)
         
     }
 
@@ -77,7 +126,47 @@ class IngredientAddTableViewController: UITableViewController {
         
     }
     
-
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            let ingredientName = ingredient!["ingredientName"] as? String
+            let unitOfMeasurement = ingredient!["unitOfMeasurement"] as? String
+            return ingredientName! + " (" + unitOfMeasurement! + ")" as? String
+        } else {
+            return "Nutrition Facts"
+        }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        
+        if section == 0 {
+            return 2
+        } else {
+                return 9
+            }
+    }
+  
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ingredientNutritionData", forIndexPath: indexPath)
+        let row = indexPath.row
+        let calories = ingredient!["calories"] as! Int
+        let caloriesAsString = String(calories)
+        
+        if row == 0 {
+            cell.textLabel?.text = caloriesAsString
+        }
+        
+        print(self.ingredient!["calories"])
+        
+        return cell
+    }
+    
+*/
     
     
 }
